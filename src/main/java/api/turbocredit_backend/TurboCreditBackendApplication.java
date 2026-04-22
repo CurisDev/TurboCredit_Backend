9a50872b35e98b19c357a1aa5c0fdae667289858
Paulu27 <pauu.lu27@gmail.com>
2026-04-21 22:07:00 -0500

    feat: Estructura Spring Boot

---
+++ src/main/java/api/turbocredit_backend/TurboCreditBackendApplication.java
@@ -0,0 +1,13 @@
+package api.turbocredit_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurboCreditBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurboCreditBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/turbocredit_backend/TurboCreditBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package api.turbocredit_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurboCreditBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
